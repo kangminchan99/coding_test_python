19dc823ffe479e08e05e209eee2e9d445a087481
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2024-03-18 17:38:14 +0900

    Lv0

---
+++ 프로그래머스/Lv0 - 카운트 업.py
@@ -0,0 +1,7 @@
+def solution(start_num, end_num):
+    answer = []
+#     start 부터 end까지 answer에 append
+    for i in range(start_num, end_num + 1):
+        answer.append(i)
+    return answer
+        
