7c67b1842bef72e96bd7ae1045db0a9f7571ae89
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2024-04-11 19:22:23 +0900

    4/11

---
+++ 프로그래머스/Lv1/Lv1 - 두 개 뽑아서 더하기.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+
+def solution(numbers):
+#   2개씩 리스트에 있는거 중복없이 다 더해서 리스트 형태로
+    combiList = list(combinations(numbers, 2))
+    
+    answer = []
+    
+    result = []
+    
+    for i in range(len(combiList)):
+        answer.append(combiList[i][0] + combiList[i][1])
+        
+    for i in set(answer):
+        result.append(i)
+        
+    return sorted(result)
+        
+    
