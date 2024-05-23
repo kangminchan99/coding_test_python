a6f8c92369e3d66dd16bad56bcc25661814d74fa
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2024-05-23 18:08:56 +0900

    5/23

---
+++ 프로그래머스/Lv0/Lv0 - 영어가 싫어요.py
@@ -0,0 +1,13 @@
+def solution(numbers):
+    dic = {"zero": 0, "one" : 1, "two" : 2, "three" : 3, "four" : 4, "five" : 5, "six" : 6, "seven" : 7, "eight": 8, "nine" : 9}
+    answer = ''
+    
+    result = ''
+    for i in numbers:
+        answer += i
+        if answer in dic:
+            result += str(dic[answer])
+            answer = ''
+    
+    
+    return int(result)
